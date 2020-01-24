a5f1adcd77766b37e71c11ef912bef057f79dd2c
Noodles <charles.pitek@gmail.com>
2020-01-24 10:01:57 -0700

    created 3 more routes for the api get post and delete, filled in get and post routes

--- server.js
+++ server.js
@@ -7,12 +7,30 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+const notes = [];
+
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "public/notes.html"))
+});
+
+app.get("/api/notes", function(req, res){
+    return res.json(notes);
+});
+
+app.post("/api/notes", function(req, res){
+    var newNote = req.body;
+
+    notes.push(newNote);
+
+    res.end();
+});
+
+app.delete("/api/notes", function(req, res){
+
 });
 
 app.listen(PORT, function(){
