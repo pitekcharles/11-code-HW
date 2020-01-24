304746544f0f4fdb8ee58d6d4cb07b293716c710
Noodles <charles.pitek@gmail.com>
2020-01-24 12:44:03 -0700

    finished off the functionallity, got app working

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-const notes = [];
-const id = 0;
+let notes = [];
+let id = 0;
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "public/index.html"));
@@ -26,7 +26,8 @@ app.post("/api/notes/", function(req, res){
     var newNote = req.body;
 
     newNote.id = id;
-    console.log(newNote);
+    id++;
+    // console.log(newNote);
     notes.push(newNote);
 
     res.end();
@@ -34,10 +35,15 @@ app.post("/api/notes/", function(req, res){
 
 app.delete("/api/notes/:id", function(req, res){
     var chosenID = req.params.id;
+    console.log(chosenID);
+    // console.log(notes);
 
     for (var i = 0; i < notes.length; i++){
-        if(notes[i].id === chosenID){
+        console.log(notes);
+        if(notes[i].id == chosenID){
             notes.splice(i, 1);
+            console.log("you made it to the if loop");
+            console.log(notes);
         }
     }
 
